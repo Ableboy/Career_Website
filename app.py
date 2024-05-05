fbe0876f1ad0213ca5466c95c7493bfc14c17f76
Ebube Nwoke <117966877+Ableboy@users.noreply.github.com>
2024-05-05 00:08:16 +0000

    Added Dynamic Data, API Route and also modify the About Content

--- app.py
+++ app.py
@@ -1,12 +1,54 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+# These are stored as a variable but can be stored as a database.
+JOBS = [
+  {
+    'id': '1',
+    'title': 'Data Analysis',
+    'location': 'Lagos',
+    'salary': 'N 250, 000',
+  },
+  {
+    'id': '2',
+    'title': 'Software Engineer',
+    'location': 'San Francisco, USA',
+    'salary': '$ 120, 000',
+  },
+  {
+    'id': '3',
+    'title': 'Data Scientist',
+    'location': 'Abuja',
+    'salary': 'N 500, 000',
+  },
+  {
+    'id': '4',
+    'title': 'Full-Stack Developer',
+    'location': 'Remote',
+  },
+  {
+    'id': '5',
+    'title': 'AI/ML Engineer',
+    'location': 'California, USA',
+    'salary': '$ 250, 000',
+  },
+  {
+    'id': '6',
+    'title': 'Cloud Engineer',
+    'location': 'Texas, USA',
+    'salary': '$ 150, 000',
+  }
+]
 
 @app.route("/")
 def hello_world():
-  return render_template('index.html')
+  return render_template('index.html', jobs=JOBS)
 
-
+# Using Json Api to returb the dynamic job data instead of using html rendering template
+@app.route("/api/jobs")
+def job_list():
+  return jsonify(JOBS)
+  
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
